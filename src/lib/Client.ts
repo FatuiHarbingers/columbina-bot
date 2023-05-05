e721f89b8653dc977a69e371b5bc7e99430936da
bitomic <35199700+bitomic@users.noreply.github.com>
2023-05-05 12:00:49 -0600

    chore: setup repository

---
+++ src/lib/Client.ts
@@ -0,0 +1,30 @@
+import { LogLevel, SapphireClient } from '@sapphire/framework'
+import { env } from './environment'
+import { Intents } from 'discord.js'
+import type { Sequelize } from 'sequelize'
+
+export class UserClient extends SapphireClient {
+	public constructor() {
+		super( {
+			defaultPrefix: env.DISCORD_PREFIX ?? '!',
+			intents: [
+				Intents.FLAGS.GUILDS,
+				Intents.FLAGS.GUILD_MESSAGES
+			],
+			loadDefaultErrorListeners: true,
+			logger: {
+				level: LogLevel.Info
+			}
+		} )
+	}
+
+	public async start(): Promise<void> {
+		await this.login( env.DISCORD_TOKEN )
+	}
+}
+
+declare module '@sapphire/pieces' {
+	interface Container {
+		sequelize: Sequelize
+	}
+}
---
+++ src/listeners/Ready.ts
@@ -0,0 +1,13 @@
+import { ApplyOptions } from '@sapphire/decorators'
+import { Listener } from '@sapphire/framework'
+import type { ListenerOptions } from '@sapphire/framework'
+
+@ApplyOptions<ListenerOptions>( {
+	event: 'ready',
+	once: true
+} )
+export class UserEvent extends Listener {
+	public async run(): Promise<void> {
+		this.container.logger.info( `Ready! as ${ this.container.client.user?.tag ?? 'unknown user' }` )
+	}
+}
